bcacba939147bc1c4bb8c168f49242077f1bcd74
projects-demo <saurabh.gupta940@hcl.com>
2023-05-01 14:55:12 +0000

    container limits

--- src/main/java/com/pnm/kube/canary/HelloService.java
+++ src/main/java/com/pnm/kube/canary/HelloService.java
@@ -3,7 +3,6 @@ package com.pnm.kube.canary;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HelloService {	
 		 @RequestMapping("/svc")
