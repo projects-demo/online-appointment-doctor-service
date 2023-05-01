3732b8ceddc9fd85cbe0e78bed431efce730c074
projects-demo <saurabh.gupta940@hcl.com>
2023-05-01 08:37:06 +0000

    fix build deployment spec

--- src/main/java/com/pnm/kube/canary/HelloService.java
+++ src/main/java/com/pnm/kube/canary/HelloService.java
@@ -3,6 +3,7 @@ package com.pnm.kube.canary;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloService {	
 		 @RequestMapping("/hello")
