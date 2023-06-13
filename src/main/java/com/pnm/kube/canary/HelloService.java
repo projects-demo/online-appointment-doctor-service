58b2530c70dd8c6aaeb61539484b9c9dd6f7ad82
projects-demo <65770503+projects-demo@users.noreply.github.com>
2023-06-13 23:27:01 +0530

    Update HelloService.java

--- src/main/java/com/pnm/kube/canary/HelloService.java
+++ src/main/java/com/pnm/kube/canary/HelloService.java
@@ -3,11 +3,18 @@ package com.pnm.kube.canary;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HelloService {	
 		 @RequestMapping("/svc")
 	    public String hello() {		 
-		      return String.format("doctor-service This is hello from doctor service!!");	     
+		    	log.debug("In Testing Logs HelloService Service. debug");
+		    	log.trace("In Testing Logs HelloService Service. trace");
+		    	log.info("In Testing Logs HelloService Service. info");
+		      return String.format("doctor-service This is hello from doctor service!!");	 
+		      
 	    }	
 }
 
