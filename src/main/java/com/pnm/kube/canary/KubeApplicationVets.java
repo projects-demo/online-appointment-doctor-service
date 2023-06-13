eb472a1f832f9bd1a2919a738c1b769ebc9b56d4
saurabh.gupta <saurabh.gupta@LP-5CD2026QTB.HCLT.CORP.HCL.IN>
2023-06-13 23:23:27 +0530

    logs config

--- src/main/java/com/pnm/kube/canary/KubeApplicationVets.java
+++ src/main/java/com/pnm/kube/canary/KubeApplicationVets.java
@@ -3,9 +3,15 @@ package com.pnm.kube.canary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class KubeApplicationVets {
 	public static void main(String[] args) {
+    	log.debug("In Testing Logs KubeApplicationVets Service. debug");
+    	log.trace("In Testing Logs KubeApplicationVets Service. trace");
+    	log.info("In Testing Logs KubeApplicationVets Service. info");
 		SpringApplication.run(KubeApplicationVets.class, args);
 	}
 
