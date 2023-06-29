1341e80fd2c42b7262b11590f02dbc4f3823c2e7
szgup10 <saurabh.gupta1@ingka.com>
2023-06-29 23:11:20 +0530

    updates

--- src/main/java/com/pnm/kube/canary/HelloService.java
+++ src/main/java/com/pnm/kube/canary/HelloService.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.opentelemetry.api.trace.StatusCode;
 
-<<<<<<< HEAD
 import io.opentelemetry.api.logs.GlobalLoggerProvider;
 import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
 import io.opentelemetry.sdk.logs.SdkLoggerProvider;
@@ -42,29 +41,11 @@ import io.opentelemetry.api.metrics.Meter;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-=======
-import lombok.extern.slf4j.Slf4j;
-
->>>>>>> branch 'main' of https://github.com/projects-demo/online-appointment-doctor-service/
 @RestController
-<<<<<<< HEAD
 //@Slf4j
 @Log4j2
 public class HelloService {
 	  private static final Logger logger = LogManager.getLogger(HelloService.class);
-=======
-@Slf4j
-public class HelloService {	
-		 @RequestMapping("/svc")
-	    public String hello() {		 
-		    	log.debug("In Testing Logs HelloService Service. debug");
-		    	log.trace("In Testing Logs HelloService Service. trace");
-		    	log.info("In Testing Logs HelloService Service. info");
-		      return String.format("doctor-service This is hello from doctor service!!");	 
-		      
-	    }	
-}
->>>>>>> branch 'main' of https://github.com/projects-demo/online-appointment-doctor-service/
 
 	 // private static final Logger logger = LoggerFactory.getLogger(MyClass.class);
 	 // private static final Tracer tracer = GlobalOpenTelemetry.getTracer("my-library-name", "1.0.0");
@@ -77,8 +58,8 @@ public class HelloService {
 
 	@RequestMapping("/svc")
 	public String hello() throws IOException {
+		//myMethod();
 		new Application().main();
-
 		log.debug("In Testing Logs HelloService Service. debug");
 		log.trace("In Testing Logs HelloService Service. trace");
 		log.info("In Testing Logs HelloService Service. info");
@@ -100,16 +81,5 @@ public class HelloService {
 	          return String.format("doctor-service This is hello from doctor service!!");
 
 	}
-/**	
-	public void myMethod() {
-	    Span span = tracer.spanBuilder("myMethod").startSpan();
-	    try {
-	      // Your code here
-	      log.info("My log message with JSON structure", Attributes.of(adRequestTypeKey, "value1", adResponseTypeKey, "value2"));
-	    } catch (Exception e) {
-	      span.recordException(e);
-	    } finally {
-	      span.end();
-	    }
-	  }*/
+
 }
